c07bf0db9de6f7b2e773d624c1a0bbc75dc13ff7
Jefferson Hernández <jeffersonh44@gmail.com>
2016-03-23 12:51:38 -0500

    fix topic on admin view

---
+++ src/java/DataAccess/DAO/TopicFacade.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAccess.DAO;
+
+import DataAccess.Entity.Topic;
+import DataAccess.DAO.AbstractFacade;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Jefferson
+ */
+@Stateless
+public class TopicFacade extends AbstractFacade<Topic> {
+
+    @PersistenceContext(unitName = "BancoPersistenceU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TopicFacade() {
+        super(Topic.class);
+    }
+    
+}
